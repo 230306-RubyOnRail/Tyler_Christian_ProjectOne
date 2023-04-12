472c159fbda14aa2594f949ca7c62874180ebd6c
TDK <name20411@gmail.com>
2023-04-12 15:14:16 -0400

    Commit_April 12th 3_15pm

--- app/controllers/concerns/authenticatible.rb
+++ app/controllers/concerns/authenticatible.rb
@@ -3,7 +3,7 @@
 class Authenticatible
     def Authenticate(userGuy, token)
        #the idea is that there is a token here that is a basic rough thing;
-       #  
+       #ide  
        
     end
     def tokencheck(ider)
