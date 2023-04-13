f0a1077701b6e1dcf1443d2bda0ec55ea897eb86
TDK <name20411@gmail.com>
2023-04-12 21:56:13 -0400

    Commit_April 12th 9_56pm

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,24 +1,28 @@
-require 'BCrypt'
+#require 'BCrypt'
 class User < ApplicationRecord
-  include BCrypt
+  #include BCrypt
   
   has_many :reimbursement_lists, dependent: :destroy #<--- might need to rename these things;
   has_one :token, dependent: :destroy
   #validates :name, presence: true
-  attr_accessor :user_name, :user_id, :user_password, :usr_type
+  # attr_accessor :user_name, :user_id, :user_password, :user_type
   validates :user_name, presence: true, uniqueness: true
   validates :user_type, presence: true
   validates :user_password, presence: true, length: { minimum: 6 }
-  has_secure_password :user_password
+  puts "HEY THERE !!!!!!!-------"
+  #has_secure_password 
+  #has_secure_password :user_password
   self.primary_key="user_id"
-  
-  def authenticate(pWord)
-    pWord == user_password
-  end
-  def confirm?
-    confirmed_at?
-  end
-  def testr
-    puts "USER NAME IS : #{self.user_name} XXXXXX---XXXX"
-  end
+  # def user_password_digest()
+  # end
+
+  # def authenticate(pWord)
+  #   pWord == user_password
+  # end
+  # def confirm?
+  #   confirmed_at?
+  # end
+  # def testr
+  #   puts "user_id: #{self.user_id} user_name: #{self.user_name} user_password: #{self.user_password} user_type #{self.user_type}"
+  # end
 end
