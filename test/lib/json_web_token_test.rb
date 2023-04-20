c57adcf657674f2947b8b0c1ded7772c3891aad2
christianloring <christianrloring@gmail.com>
2023-04-20 15:11:12 -0400

    Added a simple test for automated testing

--- test/lib/json_web_token_test.rb
+++ test/lib/json_web_token_test.rb
@@ -4,18 +4,4 @@ class JsonWebTokenTest < ActiveSupport::TestCase
     test 'the truth' do
         assert true
     end
-
-    test 'encode' do
-        subject = { 'foo' => 'bar' }
-        token = JsonWebToken.encode(subject)
-        assert token.is_a?(String)
-    end
-
-    def self.decode(token)
-        JWT.decode(token, Rails.application.credentials.jwt_token_secret, true, { algorithm: 'HS256' })[0]
-      rescue JWT::ExpiredSignature, JWT::VerificationError => e
-        raise JWT::ExpiredSignature, e.message
-      rescue JWT::DecodeError, JWT::VerificationError => e
-        raise JWT::DecodeError, e.message
-      end
 end
