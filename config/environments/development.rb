8620a7e9f1ac0f3fafcd84c61b3d0d45f6884b24
christianloring <christianrloring@gmail.com>
2023-04-20 21:06:20 -0400

    Added EC2 to development.rb

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -67,4 +67,6 @@ Rails.application.configure do
 
   # Uncomment if you wish to allow Action Cable access from any origin.
   # config.action_cable.disable_request_forgery_protection = true
+  config.hosts << "ec2-3-132-179-108.us-east-2.compute.amazonaws.com"
+
 end
